f5d20c270d52b5f769642e60d54ed88728385c6e
moni-que <monicahjustus@gmail.com>
2022-05-15 13:22:12 +0300

    create posts

--- run.py
+++ run.py
@@ -2,13 +2,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
+posts = [
+
+    {
+        'author': 'shee',
+        'title': 'first opinion',
+        'contents': 'firstcontent',
+        'date_posted': '1 April 2018'
+    },
+    {
+        'author': 'Kheshi',
+        'title': 'second opinion',
+        'content': 'secondcontent',
+        'date_posted': '10 April 2018'
+    }
+
+]
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts = posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == '__main__':
