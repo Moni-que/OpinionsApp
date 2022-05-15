265b74f4aa818c901cfcc806faa5ab717606108f
moni-que <monicahjustus@gmail.com>
2022-05-15 16:41:07 +0300

    add get and post methods

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+from crypt import methods
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='6c25c3e654c961bf5e1b1b54be43bc9d'
 
 
 
@@ -27,6 +31,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title = 'About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
 
 
 if __name__ == '__main__':
