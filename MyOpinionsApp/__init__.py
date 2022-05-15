29b32fba9d64eb8db718b065a3c7f33361e2f43b
moni-que <monicahjustus@gmail.com>
2022-05-16 02:34:36 +0300

    import bcrypt

--- MyOpinionsApp/__init__.py
+++ MyOpinionsApp/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='6c25c3e654c961bf5e1b1b54be43bc9d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from MyOpinionsApp import routes
