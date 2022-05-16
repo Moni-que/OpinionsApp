2dab0081930f2ea65516b2a55634ee0c1377376f
moni-que <monicahjustus@gmail.com>
2022-05-16 03:29:47 +0300

    install flask_login

--- MyOpinionsApp/__init__.py
+++ MyOpinionsApp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config['SECRET_KEY']='6c25c3e654c961bf5e1b1b54be43bc9d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from MyOpinionsApp import routes
